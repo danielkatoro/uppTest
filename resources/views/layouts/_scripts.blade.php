52e22c641f06eca2e6703bb73ab88074a83a5916
Danny Katoro <danielkatoro@gmail.com>
2022-03-01 10:16:55 +0100

    update done

--- resources/views/layouts/_scripts.blade.php
+++ resources/views/layouts/_scripts.blade.php
@@ -6,3 +6,10 @@
     <script src="{{asset('admin/js/bundles/apexcharts/apexcharts.min.js') }}"></script>
     <script src="{{asset('admin/js/pages/index.js') }}"></script>
     <script src="{{asset('admin/js/pages/todo/todo.js') }}"></script>
+
+    <script src="{{asset('admin/js/form.min.js') }}"></script>
+    <script src="{{asset('admin/js/bundles/multiselect/js/jquery.multi-select.js') }}"></script>
+    <script src="{{asset('admin/js/bundles/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js') }}"></script>
+    <!-- Custom Js -->
+    <script src="{{asset('admin/js/admin.js') }}"></script>
+    <script src="{{asset('admin/js/pages/forms/advanced-form-elements.js') }}"></script>
