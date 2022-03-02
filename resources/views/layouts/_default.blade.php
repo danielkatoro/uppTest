669444f194ba1d7895b195e9e168d136c69005d8
Danny Katoro <danielkatoro@gmail.com>
2022-03-02 12:07:57 +0100

    done customises

--- resources/views/layouts/_default.blade.php
+++ resources/views/layouts/_default.blade.php
@@ -96,30 +96,30 @@
                         </div>
                         <div class="profile-usertitle">
                             <div class="sidebar-userpic-name"> {{ Auth::user()->name}} </div>
-                            <div class="profile-usertitle-job ">Admin </div>
+                            <div class="profile-usertitle-job ">Administrateur </div>
                         </div>
                     </li>
                    <hr>
                     <li>
                         <a href="{{route('home')}}">
-                            <i data-feather="calendar"></i>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg>
                             <span>Dashbord</span>
                         </a>
                     </li>
                         <li>
-                            <a href="{{ route('principal_banner')}}"><i data-feather="calendar"></i> <span>Principal Banner</a>
+                            <a href="{{ route('principal_banner')}}"><i data-feather="calendar"></i> <span>Bannier Principale</a>
                         </li>
                         <li>
-                            <a href="{{route('crypto_slide')}}"><i data-feather="calendar"></i> <span>Crypto slide</a>
+                            <a href="{{route('crypto_slide')}}"><i data-feather="calendar"></i> <span>Slide des Crypto</a>
                         </li>
                         <li>
-                            <a href="{{route('buy_and_sell')}}"><i data-feather="calendar"></i> <span>Buy and Sell Section</a>
+                            <a href="{{route('buy_and_sell')}}"><i data-feather="calendar"></i> <span>Section Achat et Vente</a>
                         </li>
                         <li>
-                            <a href="{{route('parteners_logo')}}"><i data-feather="calendar"></i> <span>Parteners Logo</a>
+                            <a href="{{route('parteners_logo')}}"><i data-feather="calendar"></i> <span>Logo des partenaires</a>
                         </li>
                         <li>
-                            <a href="{{route('download_app')}}"><i data-feather="calendar"></i> <span>Download App Section</a>
+                            <a href="{{route('download_app')}}"><i data-feather="calendar"></i> <span>Section Télécharger App</a>
                         </li>
                         <li>
                             <a href="{{route('amazing_features')}}"><i data-feather="calendar"></i> <span>Amazing Features Section</a>
