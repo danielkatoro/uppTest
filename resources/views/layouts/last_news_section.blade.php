2248781808ebd8e70b0210ff39b51c0910c54d5f
Danny Katoro <danielkatoro@gmail.com>
2022-03-02 09:26:10 +0100

    laste news section custom

--- resources/views/layouts/last_news_section.blade.php
+++ resources/views/layouts/last_news_section.blade.php
@@ -1,4 +1,4 @@
-@extends('./layouts._default',['title'=>'App_Config'])
+@extends('./layouts._default',['title'=>'Laste News'])
 
 @section('content')
 
@@ -9,16 +9,8 @@
                     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                         <ul class="breadcrumb breadcrumb-style ">
                             <li class="breadcrumb-item">
-                                <h4 class="page-title">All Employees</h4>
+                                <h4 class="page-title">Last news</h4>
                             </li>
-                            <li class="breadcrumb-item bcrumb-1">
-                                <a href="../../index.html">
-                                    <i class="fas fa-home"></i> Home</a>
-                            </li>
-                            <li class="breadcrumb-item bcrumb-2">
-                                <a href="#" onClick="return false;">Employee</a>
-                            </li>
-                            <li class="breadcrumb-item active">All Employees</li>
                         </ul>
                     </div>
                 </div>
@@ -27,9 +19,6 @@
                 <div class="col-lg-12 col-md-12 col-sm-12">
                     <div class="card">
                         <div class="header">
-                            <h2>
-                                <strong>All</strong> Testimonials
-                            </h2>
                             <ul class="header-dropdown m-r--5">
                                 <li class="dropdown">
                                     <a href="#" onClick="return false;" class="dropdown-toggle"
@@ -37,17 +26,6 @@
                                         aria-expanded="false">
                                         <i class="material-icons">more_vert</i>
                                     </a>
-                                    <ul class="dropdown-menu float-end">
-                                        <li>
-                                            <a href="#" onClick="return false;">Action</a>
-                                        </li>
-                                        <li>
-                                            <a href="#" onClick="return false;">Another action</a>
-                                        </li>
-                                        <li>
-                                            <a href="#" onClick="return false;">Something else here</a>
-                                        </li>
-                                    </ul>
                                 </li>
                             </ul>
                         </div>
